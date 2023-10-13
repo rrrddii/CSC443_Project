300e34bb70e74f2d057989cebaaac86987697dbc
Ridwan Abdi <94082334+rrrddii@users.noreply.github.com>
2023-10-13 10:28:58 -0500

    Adding project files

---
+++ PassGenerator/server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const port = 3000;
+
+// Serve static files from the 'public' directory
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Handle requests to the root path by serving the index.html file
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
